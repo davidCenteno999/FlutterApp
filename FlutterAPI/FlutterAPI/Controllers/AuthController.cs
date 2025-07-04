fece71f9b2f8781442bbb9c0ae5aeb630b032484
David Centeno Araya <100227276+davidCenteno999@users.noreply.github.com>
2025-07-03 19:48:17 -0600

    Init proyect

---
+++ FlutterAPI/FlutterAPI/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using FlutterAPI.Entities;
+using FlutterAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlutterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto userDto)
+        {
+            if (userDto == null || string.IsNullOrEmpty(userDto.Username) || string.IsNullOrEmpty(userDto.Password) || string.IsNullOrEmpty(userDto.Email))
+            {
+                return BadRequest("Invalid user data.");
+            }
+            
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, userDto.Password);
+
+            user.Username = userDto.Username;
+            user.PasswordHash = hashedPassword;
+            user.Email = userDto.Email;
+            // Simulate saving to a database
+            // In a real application, you would use a service to handle database operations.
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto userDto)
+        {
+            if (userDto == null || string.IsNullOrEmpty(userDto.Email) || string.IsNullOrEmpty(userDto.Password))
+            {
+                return BadRequest("Invalid login data.");
+            }
+
+            
+
+
+            // Simulate checking the user credentials
+            if (user.Email == userDto.Email && new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, userDto.Password) == PasswordVerificationResult.Success)
+            {
+                return Ok("Login successful");
+            }
+            else
+            {
+                return BadRequest("Invalid email or password.");
+            }
+        }
+    }
+}
---
+++ FlutterAPI/FlutterAPI/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace FlutterAPI.Entities
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
